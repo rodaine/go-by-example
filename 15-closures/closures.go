36892cc03a3a4f2a24d0f97c812740a665851a3d
Chris Roche <chris@vsco.co>
2015-02-04 13:24:33 -0800

    Closures

---
+++ 15-closures/closures.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// oh this could get evil:
+// func deeep() func() func() func(i int) string {}
+// T.T
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i += 1
+		return i
+	}
+}
+
+func main() {
+	nextInt := intSeq()
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInts := intSeq()
+	fmt.Println(newInts())
+}
