74d34514cfcd3fc86cfda39b657d64538b76fa5e
Chris Roche <chris@vsco.co>
2015-02-13 16:30:20 -0800

    Typo

--- 21-errors/errors.go
+++ 21-errors/errors.go
@@ -7,7 +7,7 @@ import (
 
 func f1(arg int) (int, error) {
 	if arg == 42 {
-		return -1, errors.New("Can't workth with 42")
+		return -1, errors.New("Can't work with 42")
 	}
 	return arg + 3, nil
 }
